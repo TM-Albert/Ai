37746d1341a8e815c8cbedec257a263c75b20c31
LateForever <albertgrzegrzolka124@gmail.com>
2023-03-22 23:03:17 +0100

    for notation

--- neurons.py
+++ neurons.py
@@ -88,3 +88,37 @@ n_out = [
     ]
 
 print(n_out)
+
+
+inputs = [1, 2, 3, 2.5]
+
+weights = [
+    [0.2, 0.8, -0.5, 1.0],
+    [0.5, -0.91, 0.26, -0.5],
+    [-0.26, -0.27, 0.17, 0.87]]
+
+biases = [2, 3, 0.5]
+
+outputs = []
+
+for n, b in zip(weights, biases):
+    
+    neuron = 0
+    
+    for inpt, w in zip(inputs, n):
+        
+        neuron += inpt * w
+        
+    neuron += b
+    outputs.append(neuron)
+    
+print(outputs)
+    
+
+
+
+
+
+
+
+
