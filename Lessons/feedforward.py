2f0892896eb525fe2fae3893fb1d78df8e7cd3e3
LateForever <albertgrzegrzolka124@gmail.com>
2023-10-18 09:39:11 +0200

    small changes in ai

--- Lessons/feedforward.py
+++ Lessons/feedforward.py
@@ -1,16 +1,7 @@
 from mathhelper import sigmoid
 
 def feedforward(x, weights, bias):
-    w_sum = [0, 0]
 
-    for i in range(len(x)):
-        for j in range(len(weights)):
-            w_sum[i] = x[i] * weights[j]
-
-        w_sum[i] += bias
-
-    for i in range(len(w_sum)):
-        w_sum[i] = sigmoid(w_sum[i])
 
     [0,0]
     [0.4051257858555448, -0.5843519159246258, 0.703376365781059, -0.8848898688558118]
--- Lessons/lesson1.py
+++ Lessons/lesson1.py
@@ -8,20 +8,25 @@ epochs = 10000
 input_data_set = [[0 ,0], [0, 1], [1, 0], [1, 1]]
 output_data_set = [0,0,0,1]
 
-def fit(data, y, learn_rate, epochs):
+def fit(data, output_data_set, learn_rate, epochs):
 
-    weights = [[random.uniform(-1, 1) for _ in range(2)], [random.uniform(-1, 1) for _ in range(2)]]
-    biases = [[random.uniform(-1, 1)], [random.uniform(-1, 1)]]
+    weights = [[random.uniform(-1, 1) for _ in range(2)], [random.uniform(-1, 1) for _ in range(2)], [random.uniform(-1, 1) for _ in range(2)]]
+    biases = [[random.uniform(-1, 1)], [random.uniform(-1, 1)], [random.uniform(-1, 1)]]
 
     for epoch in range(epochs):
-        for i in range(len(input_data_set)):
+        for i in range(len(data)):
 
-            x = input_data_set[i]
+            x = data[i]
             target = output_data_set[i]
 
             [0,0]
-            [0.4051257858555448, -0.5843519159246258, 0.703376365781059, -0.8848898688558118]
+            [[0.4051257858555448, -0.5843519159246258], [0.703376365781059, -0.8848898688558118]]
 
-            calculated_output = feedforward(x, weights, biases)
+            first_layer_output = feedforward(x, weights, biases)
 
             [0.8378112032793879, 0.3378112032793879]
+
+            neuron1 = first_layer_output[0]
+            neuron2 = first_layer_output[1]
+
+            output = feedforward(first_layer_output, second_layer_weights, biases[2])
